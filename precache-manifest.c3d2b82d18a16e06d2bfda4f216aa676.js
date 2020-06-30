24304e7c5cf84beedd76afd1e5d508bb01d1db93
Sergey Kovalskiy <sergey@thorgate.eu>
2020-06-30 11:56:07 +0300

    Updates

---
+++ precache-manifest.c3d2b82d18a16e06d2bfda4f216aa676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0aa08f07c2713de3855e097b437325",
+    "url": "/humble-character-generator/index.html"
+  },
+  {
+    "revision": "c6ae66a7be11c388c7b3",
+    "url": "/humble-character-generator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "efdccf646269c8f4235d",
+    "url": "/humble-character-generator/static/js/2.a56f28c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/humble-character-generator/static/js/2.a56f28c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ae66a7be11c388c7b3",
+    "url": "/humble-character-generator/static/js/main.eb0534e2.chunk.js"
+  },
+  {
+    "revision": "8f6d910fbd10bbb73dc2",
+    "url": "/humble-character-generator/static/js/runtime-main.abef53bc.js"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/humble-character-generator/static/media/github.07883e93.svg"
+  }
+]);
